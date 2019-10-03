a8321ea73c35d92a1a802de9871b24971ef8ec45
zh1014 <964701944@qq.com>
2019-10-03 14:55:15 +0800

    add Detect Directed Cycle

--- graphs/digraph/directedCycle.go
+++ graphs/digraph/directedCycle.go
@@ -1,16 +1,32 @@
 package digraph
 
-type DirectedCycleDetector struct {
+import "github.com/zh1014/algorithm/set"
+
+func DetectDirCycle(g Digraph) bool {
+	marked := make([]bool, g.NumV())
+	s := set.New()
+	for i, b := range marked {
+		if !b {
+			if detectDirCycle(g, i, marked, s) {
+				return true
+			}
+		}
+	}
+	return false
 }
 
-func NewDirectedCycleDetector(g Digraph) *DirectedCycleDetector {
-
-}
-
-func (dcd *DirectedCycleDetector) HasDirectedCycle() bool {
-
-}
-
-func HasDirectedCycle(g Digraph) bool {
-
+func detectDirCycle(g Digraph, v int, marked []bool, s set.Set) bool {
+	if s.Contains(v) {
+		return true
+	}
+	s.Add(v)
+	marked[v] = true
+	adj := g.Adjacent(v)
+	for _, w := range adj {
+		if !marked[w] && detectDirCycle(g, w, marked, s) {
+			return true
+		}
+	}
+	s.Remove(v)
+	return false
 }
