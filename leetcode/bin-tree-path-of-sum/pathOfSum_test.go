5e87b4b2d5918b74070a751a2d20bad7743c5708
zh1014 <964701944@qq.com>
2019-11-13 22:32:32 +0800

    add print-path-of-sum

---
+++ leetcode/bin-tree-path-of-sum/pathOfSum_test.go
@@ -0,0 +1,51 @@
+package pathofsum
+
+import "testing"
+
+func TestPathOfSum(t *testing.T) {
+	leaf1 := &node{
+		v:     1,
+	}
+	leaf2 := &node{
+		v:     2,
+	}
+	leaf3 := &node{
+		v:     3,
+	}
+	h1node1 := &node{
+		v:     3,
+		left:  leaf1,
+		right: leaf2,
+	}
+	h1node2 := &node{
+		v:     20,
+		left:  leaf1,
+		right: leaf2,
+	}
+	h1node3 := &node{
+		v:     5,
+		left:  leaf1,
+		right: leaf3,
+	}
+	h1node4 := &node{
+		v:     6,
+		left:  leaf1,
+		right: leaf2,
+	}
+	h2node1 := &node{
+		v:     -11,
+		left:  h1node1,
+		right: h1node2,
+	}
+	h2node2 := &node{
+		v:     4,
+		left:  h1node3,
+		right: h1node4,
+	}
+	tree := &node{
+		v:     8,
+		left:  h2node1,
+		right: h2node2,
+	}
+	printPathOfSum(tree, 18, make([]int, 0, 3))
+}
