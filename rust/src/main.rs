75113e2aafb66400bfe7e3efecd4fc546f09b611
howz <964701944@qq.com>
2021-06-23 10:49:36 +0800

    mean median mode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,4 @@
-// use std::collections::HashMap;
-
-fn main() {
-    // let ve = vec![2,2,2,0,1];
-}
+use std::collections::HashMap;
 
 // fn num_way(n: i32) -> i32 {
 //     if n == 0 || n == 1 {
@@ -65,3 +61,63 @@ fn main() {
 //     }
 //     return -1
 // }
+
+fn main() {
+    let mut numbers = vec![0,1,1,2,3,4,5,5,5,6,7,8,9,0,0];
+    let mean = calc_mean(&numbers);
+    match mean {
+        Some(i) => println!("mean is {}", i),
+        None => println!("mean not exist")
+    }
+    let median = calc_median(&mut numbers);
+    match median {
+        Some(i) => println!("median is {}", i),
+        None => println!("median not exist")
+    }
+    let mode = calc_mode(&numbers);
+    match mode {
+        Some(i) => println!("mode is {}", i),
+        None => println!("mode not exist")
+    }
+}
+
+fn calc_mean(numbers: &Vec<i32>) -> Option<i32> {
+    if numbers.len() == 0 {
+        return None
+    }
+    let mut sum = 0;
+    for number in numbers {
+        sum += number
+    }
+    Some(sum/(numbers.len() as i32))
+}
+
+fn calc_median(numbers: &mut Vec<i32>) -> Option<&i32> {
+    let length = numbers.len();
+    if length % 2 == 0 {
+        return None
+    }
+    numbers.sort();
+    let median_idx = length / 2;
+    numbers.get(median_idx)
+}
+
+fn calc_mode(numbers: &Vec<i32>) -> Option<i32> {
+    if numbers.len() == 0 {
+        return None
+    }
+    let mut count_map = HashMap::new();
+    for number in numbers {
+        let count = count_map.entry(*number).or_insert(0);
+        *count += 1;
+    }
+    let mut count_max = 0;
+    let mut mode = 0;
+    for (number, count) in count_map {
+        if count > count_max {
+            count_max = count;
+            mode = number;
+        }
+    }
+    Some(mode)
+}
