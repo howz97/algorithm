aaf3ec2961765749796c2d7f1425939d7e3f5b7a
howz97 <964701944@qq.com>
2021-11-23 18:47:57 +0800

    fix demo

--- integer_test/w_digraph.go
+++ integer_test/w_digraph.go
@@ -3,15 +3,13 @@ package main
 import wDigraph "github.com/howz97/algorithm/graphs/weighted_digraph"
 
 func main() {
-	g, err := wDigraph.ImportEWD("/Users/zhanghao1/code/algorithm/integer_test/tinyEWD.txt")
+	g, err := wDigraph.ImportEWD("../graphs/weighted_digraph/tinyEWD.txt")
 	if err != nil {
 		panic(err)
 	}
 	pathSearcher, err := g.GenSearcherDijkstra()
-	/*
-		pathSearcher, err := g.GenSearcherTopological()
-		pathSearcher, err := g.GenSearcherBellmanFord()
-	*/
+	//pathSearcher, err := g.GenSearcherTopological()
+	//pathSearcher, err := g.GenSearcherBellmanFord()
 	if err != nil {
 		panic(err)
 	}
