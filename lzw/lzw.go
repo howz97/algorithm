c710a21e9b9f3491021207ade7312719c01ce3fe
howz97 <964701944@qq.com>
2021-12-25 16:09:24 +0800

    fix

--- lzw/lzw.go
+++ lzw/lzw.go
@@ -7,21 +7,31 @@ import (
 
 func Compress(data []byte) (out []byte) {
 	table := hash_map.New()
-	var (
-		key  []byte
-		code uint16
-	)
-	for _, b := range data {
-		key = append(key, b)
-		e := table.Get(Str(key))
-		if e != nil {
-			continue
+	unused := uint16(1)
+	for i := 0; i <= 0xFF; i++ {
+		table.Put(Str([]byte{byte(i)}), unused)
+		unused++
+	}
+	for len(data) > 0 {
+		var code uint16
+		i := 1
+		for ; i <= len(data); i++ {
+			e := table.Get(Str(data[:i]))
+			if e == nil {
+				break
+			}
+			code = e.(uint16)
 		}
-		code++
-		table.Put(Str(key), code)
-		key = key[:0]
+		if i > len(data) {
+			out = append(out, byte(code))
+			out = append(out, byte(code>>8))
+			break
+		}
+		table.Put(Str(data[:i]), unused)
+		unused++
 		out = append(out, byte(code))
 		out = append(out, byte(code>>8))
+		data = data[i-1:]
 	}
 	return
 }
