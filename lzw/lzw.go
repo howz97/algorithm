1395bc889ca6f4dadcfd25221405014810f9001a
howz97 <964701944@qq.com>
2021-12-25 15:19:15 +0800

    lzw

--- lzw/lzw.go
+++ lzw/lzw.go
@@ -1,7 +1,29 @@
 package lzw
 
-func Compress(data []byte) []byte {
+import (
+	"github.com/howz97/algorithm/search/hash_map"
+	. "github.com/howz97/algorithm/util"
+)
 
+func Compress(data []byte) (out []byte) {
+	table := hash_map.New()
+	var (
+		key  []byte
+		code uint16
+	)
+	for _, b := range data {
+		key = append(key, b)
+		e := table.Get(Str(key))
+		if e != nil {
+			continue
+		}
+		code++
+		table.Put(Str(key), code)
+		key = key[:0]
+		out = append(out, byte(code))
+		out = append(out, byte(code>>8))
+	}
+	return
 }
 
 func Decompress(data []byte) []byte {
