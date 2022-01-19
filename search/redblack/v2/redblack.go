af64a4836807e6e6a35342a6e3830ec23f3ff901
howz97 <964701944@qq.com>
2022-01-19 21:36:06 +0800

    redblack

--- search/redblack/v2/redblack.go
+++ search/redblack/v2/redblack.go
@@ -1,7 +1,7 @@
 package v2
 
 import (
-	"github.com/howz97/algorithm/util"
+	. "github.com/howz97/algorithm/util"
 )
 
 const (
@@ -9,226 +9,172 @@ const (
 	black = false
 )
 
-// RedBlack is a dictionary implemented by read-black tree
-type RedBlack struct {
-	root *node
-}
-
-// New new an empty read-black tree
-func New() *RedBlack {
-	return new(RedBlack)
-}
-
-// Insert insert a k-v pair into this dictionary
-// if value is nil, is is a vitual deletion operation
-func (rb *RedBlack) Insert(key util.Comparable, value util.T) {
-	rb.root = rb.root.insert(key, value)
-	if rb.root.isRed() {
-		rb.root.color = black
-	}
-}
-
-func (rb *RedBlack) Find(key util.Comparable) util.T {
-	n := rb.root.find(key)
-	if n == nil {
-		return nil
-	}
-	return n.value
-}
-
-func (rb *RedBlack) FindMin() util.T {
-	return rb.root.findMin().value
-}
-
-func (rb *RedBlack) FindMax() util.T {
-	return rb.root.findMax().value
-}
-
-func (rb *RedBlack) DelMin() {
-	if rb.root == nil {
-		return
-	}
-	if !rb.root.leftSon.isRed() && !rb.root.rightSon.isRed() {
-		rb.root.color = red
-	}
-	rb.root = rb.root.delMin()
-	if rb.root != nil {
-		rb.root.color = black
-	}
-}
-
-// Empty return true if it is an empty tree
-func (rb *RedBlack) Empty() bool {
-	return rb.root == nil
-}
-
-func (rb *RedBlack) Size() int {
-	if rb.root == nil {
-		return 0
-	}
-	return rb.root.size
-}
-
-/*=============================================================================*/
-
 type node struct {
-	key      util.Comparable
-	value    util.T
-	color    bool
-	size     int
-	leftSon  *node
-	rightSon *node
+	key            Comparable
+	value          T
+	color          bool
+	p, left, right *node
 }
 
-func (n *node) insert(k util.Comparable, v util.T) *node {
-	if n == nil {
-		return &node{
-			key:   k,
-			value: v,
-			color: red,
-			size:  1,
+func (n *node) Cmp(a Comparable) Result {
+	return n.key.Cmp(a.(*node).key)
+}
+
+func (n *node) getMin() *node {
+
+}
+
+type Tree struct {
+	root *node
+	null *node
+}
+
+func (tree *Tree) Put(key Comparable, val T) {
+	p := tree.null
+	x := tree.root
+	for x != tree.null {
+		p = x
+		switch key.Cmp(x.key) {
+		case Less:
+			x = x.left
+		case More:
+			x = x.right
+		case Equal:
+			x.value = val
+			return
 		}
 	}
-	switch k.Cmp(n.key) {
-	case util.Less:
-		n.leftSon = n.leftSon.insert(k, v)
-	case util.More:
-		n.rightSon = n.rightSon.insert(k, v)
-	default:
-		n.value = v
+	in := &node{
+		key:   key,
+		value: val,
+		color: red,
+		p:     p,
+		left:  tree.null,
+		right: tree.null,
 	}
-	n.size = size(n.leftSon) + size(n.rightSon) + 1
-	if !n.leftSon.isRed() && n.rightSon.isRed() {
-		n = rotateLeft(n)
+	if p == tree.null {
+		tree.root = in
+	} else if in.Cmp(p) == Less {
+		p.left = in
+	} else {
+		p.right = in
 	}
-	if n.leftSon.isRed() && n.leftSon.leftSon.isRed() {
-		n = rotateRight(n)
-	}
-	if n.leftSon.isRed() && n.rightSon.isRed() {
-		flipColors(n)
-	}
-	return n
+	tree.fixInsert(in)
 }
 
-func (n *node) find(key util.Comparable) *node {
-	if n == nil {
-		return nil
-	}
-	switch key.Cmp(n.key) {
-	case util.Less:
-		return n.leftSon.find(key)
-	case util.More:
-		return n.rightSon.find(key)
-	default:
-		return n
+func (tree *Tree) fixInsert(n *node) {
+	for n.p.color == red {
+		if n.p == n.p.p.left {
+			uncle := n.p.p.right
+			if uncle.color == red {
+				n.p.color, uncle.color = black, black
+				n = n.p.p
+				n.color = red
+				// continue
+			} else {
+				// uncle is black
+				if n == n.p.right {
+					n = n.p
+					tree.leftRotate(n)
+				}
+				n.p.color = black
+				n.p.p.color = red
+				tree.rightRotate(n.p.p)
+			}
+		} else {
+			uncle := n.p.p.left
+			if uncle.color == red {
+				n.p.color, uncle.color = black, black
+				n = n.p.p
+				n.color = red
+				// continue
+			} else {
+				// uncle is black
+				if n == n.p.left {
+					n = n.p
+					tree.rightRotate(n)
+				}
+				n.p.color = black
+				n.p.p.color = red
+				tree.leftRotate(n.p.p)
+			}
+		}
 	}
 }
 
-func (n *node) findMin() *node {
-	if n == nil {
-		return nil
+func (tree *Tree) rightRotate(n *node) {
+	top := n.left
+	n.left = top.right
+	if n.left != tree.null {
+		n.left.p = n
 	}
-	for n.leftSon != nil {
-		n = n.leftSon
-	}
-	return n
+	tree.transplant(n, top)
+	top.right = n
+	n.p = top
 }
 
-func (n *node) findMax() *node {
-	if n == nil {
-		return nil
+func (tree *Tree) leftRotate(n *node) {
+	top := n.right
+	n.right = top.left
+	if n.right != tree.null {
+		n.right.p = n
 	}
-	for n.rightSon != nil {
-		n = n.rightSon
-	}
-	return n
+	tree.transplant(n, top)
+	top.left = n
+	n.p = top
 }
 
-func (n *node) delMin() *node {
-	if n.leftSon == nil {
-		return nil
+func (tree *Tree) transplant(a, b *node) {
+	b.p = a.p
+	if b.p == tree.null {
+		tree.root = b
+	} else if a == a.p.left {
+		b.p.left = b
+	} else {
+		b.p.right = b
 	}
-	if !n.leftSon.isRed() && !n.leftSon.leftSon.isRed() {
-		n = moveRedLeft(n)
-	}
-	n.leftSon = n.leftSon.delMin()
-	return balance(n)
 }
 
-func moveRedLeft(r *node) *node {
-	flipColors2(r)
-	if r.rightSon.leftSon.isRed() {
-		r.rightSon = rotateRight(r.rightSon)
-		r = rotateLeft(r)
-	}
-	return r
+func (tree *Tree) find(key Comparable) *node {
+
 }
 
-func balance(r *node) *node {
-	if r.rightSon.isRed() {
-		r = rotateLeft(r)
+func (tree *Tree) Del(key Comparable) {
+	z := tree.find(key)
+	if z == nil {
+		return
 	}
-	r.size = size(r.leftSon) + size(r.rightSon) + 1
-	if !r.leftSon.isRed() && r.rightSon.isRed() {
-		r = rotateLeft(r)
+	y := z
+	yOrig := y.color
+	var x *node
+	if z.left == tree.null {
+		x = z.right
+		tree.transplant(z, x)
+	} else if z.right == tree.null {
+		x = z.left
+		tree.transplant(z, x)
+	} else {
+		y = z.right.getMin()
+		yOrig = y.color
+		x = y.right
+		if y.p != z {
+			tree.transplant(y, x)
+			y.right = z.right
+			y.right.p = y
+		}
+		tree.transplant(z, y)
+		y.left = z.left
+		y.left.p = y
+		y.color = z.color
 	}
-	if r.leftSon.isRed() && r.leftSon.leftSon.isRed() {
-		r = rotateRight(r)
+	if yOrig == black {
+		tree.fixDelete(x)
 	}
-	if r.leftSon.isRed() && r.rightSon.isRed() {
-		flipColors(r) // ?
-	}
-	return r
 }
 
-func flipColors2(r *node) {
-	r.color = black
-	r.leftSon.color = red
-	r.rightSon.color = red
-}
+func (tree *Tree) fixDelete(n *node) {
+	for n != tree.root && n.color == black {
 
-func (n *node) isRed() bool {
-	if n == nil {
-		return false
 	}
-	return n.color == red
-}
-
-func size(n *node) int {
-	if n == nil {
-		return 0
-	}
-	return n.size
-}
-
-func rotateLeft(root *node) *node {
-	newRoot := root.rightSon
-	root.rightSon = newRoot.leftSon
-	newRoot.leftSon = root
-	root.size = size(root.leftSon) + size(root.rightSon) + 1
-	newRoot.size = size(newRoot.leftSon) + size(newRoot.rightSon) + 1
-	if root.color == black {
-		newRoot.color = black
-		root.color = red
-	}
-	return newRoot
-}
-
-func rotateRight(root *node) *node {
-	newRoot := root.leftSon
-	root.leftSon = newRoot.rightSon
-	newRoot.rightSon = root
-	root.size = size(root.leftSon) + size(root.rightSon) + 1
-	newRoot.size = size(newRoot.leftSon) + size(newRoot.rightSon) + 1
-	if root.color == black {
-		newRoot.color = black
-		root.color = red
-	}
-	return newRoot
-}
-
-func flipColors(root *node) {
-	root.leftSon.color = black
-	root.rightSon.color = black
-	root.color = red
+	n.color = black
 }
