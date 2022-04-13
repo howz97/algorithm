efba03ac010224a5b13a8a57d2b600513373e353
howz97 <964701944@qq.com>
2022-04-13 17:42:27 +0800

    fix demo

--- demo/print_bin_tree.go
+++ demo/print_bin_tree.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/howz97/algorithm/search"
 	"github.com/howz97/algorithm/search/avltree"
-	"github.com/howz97/algorithm/util"
 )
 
 var pairs = []string{
@@ -19,9 +18,9 @@ var pairs = []string{
 }
 
 func main() {
-	tree := avltree.New()
+	tree := avltree.New[float64, string]()
 	for k, v := range pairs {
-		tree.Put(util.Float(float64(k)/100), v)
+		tree.Put(float64(k)/100, v)
 	}
 	search.PrintBinaryTree(tree)
 }
