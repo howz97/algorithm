e62c8ef093699342dec20c443474518192f7b2a7
howz97 <964701944@qq.com>
2022-06-19 11:32:23 +0800

    add comment

--- sort/shell.go
+++ sort/shell.go
@@ -3,8 +3,9 @@ package sort
 import "golang.org/x/exp/constraints"
 
 func Shell[Ord constraints.Ordered](data []Ord) {
-	// 使用希尔增量
+	// shell increment
 	for incre := len(data) >> 1; incre > 1; incre >>= 1 {
+		// sort $incre arries with interval of $incre in one loop
 		for i := incre; i < len(data); i++ {
 			insrtNum := data[i]
 			j := i - incre
@@ -18,7 +19,7 @@ func Shell[Ord constraints.Ordered](data []Ord) {
 }
 
 //func ShellSort(data []int) {
-//	// 使用希尔增量
+//	// shell increment
 //	for incre := len(data) >> 1; incre > 0; incre >>= 1 {
 //		for i := incre; i < len(data); i++ {
 //			insrtNum := data[i]
